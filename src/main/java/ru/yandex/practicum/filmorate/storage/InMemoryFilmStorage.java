cc390a46946557ceb62efa8399c7d1616fe6fcf3
RuslanRinatovich <rusjlan4uk@mail.ru>
2024-08-01 17:22:28 +0300

    Изменить status-code

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import ru.yandex.practicum.filmorate.exception.IncorrectParameterException;
+import ru.yandex.practicum.filmorate.exception.InternalServerErrorException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -26,19 +27,19 @@ public class InMemoryFilmStorage implements FilmStorage {
     void validateFilmsData(Film film) throws ValidationException {
         if (film.getName() == null || film.getName().isBlank()) {
             logger.error("название не может быть пустым");
-            throw new IncorrectParameterException("название не может быть пустым");
+            throw new InternalServerErrorException("название не может быть пустым");
         }
         if (film.getDescription() != null && film.getDescription().length() > 200) {
             logger.error("максимальная длина описания — 200 символов");
-            throw new IncorrectParameterException("максимальная длина описания — 200 символов");
+            throw new InternalServerErrorException("максимальная длина описания — 200 символов");
         }
         if (film.getDuration() <= 0) {
             logger.error("продолжительность фильма должна быть положительным числом");
-            throw new IncorrectParameterException("продолжительность фильма должна быть положительным числом");
+            throw new InternalServerErrorException("продолжительность фильма должна быть положительным числом");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, Calendar.DECEMBER, 28))) {
             logger.error("дата релиза — не раньше 28 декабря 1895 года");
-            throw new IncorrectParameterException("дата релиза — не раньше 28 декабря 1895 года");
+            throw new InternalServerErrorException("дата релиза — не раньше 28 декабря 1895 года");
         }
     }
 
